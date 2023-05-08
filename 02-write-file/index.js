070e7c2f830af3f396bf892a996fa10df25525ad
Hellena16 <elena-kuzmitskaya@rambler.ru>
2023-05-08 18:57:12 +0300

    feat: solve task 2

---
+++ 02-write-file/index.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const fs = require('fs');
+
+const { stdin, stdout } = process;
+
+const stream = fs.createWriteStream(path.join(__dirname, 'text.txt'), 'utf-8');
+
+stdout.write('Hello! Please, write your text\n');
+stdin.on('data', data => {
+    if (data.toString().toLowerCase().trim() === 'exit') {
+        process.exit();
+      }
+      else {
+        stream.write(data);
+      }
+});
+process.on('exit', () => stdout.write('Bye!'));
