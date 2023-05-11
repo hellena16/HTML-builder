4d009613f8a20a8d7dbdb2b3da63b72d4cb6c113
Hellena16 <elena-kuzmitskaya@rambler.ru>
2023-05-11 16:30:05 +0300

    feat: fix 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,28 +1,33 @@
-const fs = require('fs/promises')
+const fs = require('fs')
 const path = require('path')
 
 const folder = path.join(__dirname, 'files')
 const copyFolder = path.join(__dirname, 'files-copy')
 
-async function copyDirectory(folder, copyFolder) {
+function copyDirectory(folder, copyFolder) {
   fs.mkdir(copyFolder, { recursive: true }, (err) => {
     if (err) throw err
   })
-  fs.readdir(folder, { withFileTypes: true }, (files) => {
-    files.forEach((file) => {
-      const inPath = path.join(folder, file.name)
-      const outPath = path.join(copyFolder, file.name)
-      if (file.isDirectory()) {
-        fsprom.mkdir(outPath, { recursive: true }).then(() => {
-          copyDirectory(inPath, outPath)
-        })
-      } else {
-        fs.copyFile(inPath, outPath, (err) => {
-          if (err) throw err
-        })
-      }
-    })
+  fs.readdir(folder, { withFileTypes: true }, (error, files) => {
+    if (error) {
+      console.log(error)
+    } else {
+      files.forEach((file) => {
+        const inPath = path.join(folder, file.name)
+        const outPath = path.join(copyFolder, file.name)
+        if (file.isDirectory()) {
+          fsprom.mkdir(outPath, { recursive: true }).then(() => {
+            copyDirectory(inPath, outPath)
+          })
+        } else {
+          fs.copyFile(inPath, outPath, (err) => {
+            if (err) throw err
+          })
+        }
+      })
+    }
   })
 }
+
 
 copyDirectory(folder, copyFolder)
