c0cc08e763e188d51f0e26a0d6e35d6e99c48f91
Hellena16 <elena-kuzmitskaya@rambler.ru>
2023-05-11 11:45:35 +0300

    feat: fix mistake 02 and 04

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,17 +1,21 @@
-const path = require('path');
-const fs = require('fs');
+const path = require('path')
+const fs = require('fs')
+const readline = require('readline')
 
-const { stdin, stdout } = process;
+const { stdin, stdout } = process
 
-const stream = fs.createWriteStream(path.join(__dirname, 'text.txt'), 'utf-8');
+const stream = fs.createWriteStream(path.join(__dirname, 'text.txt'), 'utf-8')
 
-stdout.write('Hello! Please, write your text\n');
-stdin.on('data', data => {
-    if (data.toString().toLowerCase().trim() === 'exit') {
-        process.exit();
-      }
-      else {
-        stream.write(data);
-      }
-});
-process.on('exit', () => stdout.write('Bye!'));
+stdout.write('Hello! Please, write your text\n')
+stdin.on('data', (data) => {
+  if (data.toString().toLowerCase().trim() === 'exit') {
+    process.exit()
+  } else {
+    stream.write(data)
+  }
+})
+process.on('exit', () => stdout.write('Bye!'))
+process.on('SIGINT', () => {
+  stdout.write('Bye!')
+  process.exit()
+})
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,16 +5,24 @@ const folder = path.join(__dirname, 'files')
 const copyFolder = path.join(__dirname, 'files-copy')
 
 async function copyDirectory(folder, copyFolder) {
-  await fs
-    .mkdir(copyFolder, { recursive: true })(
-      await fs.readdir(folder, { withFileTypes: true }),
-    )
-    .forEach((file) => {
-      fs.copyFile(
-        path.join(folder, file.name),
-        path.join(copyFolder, file.name),
-      )
+  fs.mkdir(copyFolder, { recursive: true }, (err) => {
+    if (err) throw err
+  })
+  fs.readdir(folder, { withFileTypes: true }, (files) => {
+    files.forEach((file) => {
+      const inPath = path.join(folder, file.name)
+      const outPath = path.join(copyFolder, file.name)
+      if (file.isDirectory()) {
+        fsprom.mkdir(outPath, { recursive: true }).then(() => {
+          copyDirectory(inPath, outPath)
+        })
+      } else {
+        fs.copyFile(inPath, outPath, (err) => {
+          if (err) throw err
+        })
+      }
     })
+  })
 }
 
 copyDirectory(folder, copyFolder)
